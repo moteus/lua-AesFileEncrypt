c601a9ebb5654d7882d362e702736ebe923f6502
Alexey Melnichuk <mimir@newmail.ru>
2014-02-18 14:32:40 +0400

    Fix. build for Lua 5.2 with LUA_COMPAT_XXX

--- src/l52util.h
+++ src/l52util.h
@@ -11,13 +11,17 @@
 // luaL_setfuncs
 // lua_absindex
 
+#ifndef lua_objlen
 
 #define lua_objlen      lua_rawlen
 
+#endif
 int   luaL_typerror (lua_State *L, int narg, const char *tname);
 
+#ifndef luaL_register
 void luaL_register (lua_State *L, const char *libname, const luaL_Reg *l);
 
+#endif
 #else                      // lua 5.1
 
 // functions form lua 5.2
